fb5ee0a3bd40991d02d5c13b7377854d19b5231d
Guillaume Gnaegi <guillaume.gnaegi@gmail.com>
2020-07-14 16:30:40 +0200

    Updating Package Version

--- src/js/addon.js
+++ src/js/addon.js
@@ -7,14 +7,14 @@ function FolderPermissionsManager({folderPath}){
 //parameters domain, name, accessString, isUser, propagate
 FolderPermissionsManager.prototype.addRight = function({domain, name, accessString, isUser = false, propagate = true}){
     return this.nativeFpm.addRight(domain, name, accessString, isUser, propagate);
-}
+};
 
 FolderPermissionsManager.prototype.applyRights = function({disableInheritance=false}){
     return this.nativeFpm.applyRights(disableInheritance);
-}
+};
 
 FolderPermissionsManager.prototype.clearExplicitAccessList = function(){
     return this.nativeFpm.clearExplicitAccessList();
-}
+};
 
 module.exports = FolderPermissionsManager;
